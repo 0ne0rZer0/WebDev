9ddbbdc4b87c5c70756245581b8d4db1b87650fa
Utkarsh Nagdev <utkarsh2nagdev@gmail.com>
2021-07-07 02:51:07 +0530

    React

--- FrontEnd/6.ArraysJS/1.isArray.js
+++
@@ -1,7 +0,0 @@
-// Create a function, which checks parameter is array or not
-function isArray(input) {
-  return Array.isArray(input);
-}
-console.log(isArray([1, 2, 3]));
-console.log(isArray("wwww"));
-console.log(isArray(1));
