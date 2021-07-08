e72b1cb78f82546cab21b33b8cf3e7a149dff80d
Utkarsh Nagdev <utkarsh2nagdev@gmail.com>
2021-07-09 02:47:51 +0530

    Renaming

--- FrontEnd/7.ObjectsJS/lengthOfObject.js
+++
@@ -1,9 +0,0 @@
-function lengthOfObject(student) {
-  var text = 0;
-  for (x in student) {
-    text += 1;
-  }
-  return text;
-}
-var student = { name: "David Rayy", sclass: "VI", rollno: 12 };
-console.log(lengthOfObject(student));
--- FrontEnd/7.ObjectsJS/volumeOfCylider.js
+++
@@ -1,7 +0,0 @@
-function volumeCylinder(cylinder) {
-  return (Math.PI * cylinder.radius ** 2 * cylinder.height).toFixed(4);
-}
-
-console.log(
-  "Volume of Cylinder: " + volumeCylinder({ radius: 10, height: 15 })
-);
